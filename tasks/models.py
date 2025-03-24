daf7ddbc7304ae34f51bf0b5b30e546692ea739e
volodymyr <yablonskyi.volodymyr28@gmail.com>
2025-03-24 22:41:41 +0200

    Created models Worker and Position, updated model Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -6,6 +7,25 @@ class TaskType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name="workers")
+    username = models.CharField(max_length=256)
+    email = models.EmailField()
+    password = models.CharField(max_length=256)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
 
 class Task(models.Model):
@@ -26,6 +46,7 @@ class Task(models.Model):
         default=Priority.MEDIUM
     )
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name="tasks")
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
 
     def __str__(self):
         return self.name
