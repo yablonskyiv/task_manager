e2e8c8129a4c364753e06a56c3a51184f2286e48
volodymyr <yablonskyi.volodymyr28@gmail.com>
2025-03-24 22:03:27 +0200

    Created model Task with fields

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(max_length=500)
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+
+    class Priority(models.TextChoices):
+        URGENT = "urgent", "Urgent"
+        HIGH = "hi", "High"
+        MEDIUM = "medium", "Medium"
+        LOW = "low", "Low"
+
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices,
+        default=Priority.MEDIUM
+    )
